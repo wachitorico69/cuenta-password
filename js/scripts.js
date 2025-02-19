068e7c9ce5ad734f1e4e309a88c279ae10e78487
wachitorico69 <morenodario919@gmail.com>
2025-02-19 17:33:35 -0600

    final?

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,10 @@ function guardarDatos() {
 
 function recuperarDatos() {
     if ((localStorage.nombre != undefined) && (localStorage.password != undefined)) {
-        document.getElementById("datos").innerHTML = "Nombre: " + localStorage.nombre + "Password: " + localStorage.password;
+        document.getElementById("datos").innerHTML = "Nombre: " + localStorage.nombre + "\nPassword: " + localStorage.password;
+        document.getElementById("datos").style.borderBottom = "3px solid white";
     } else {
         document.getElementById("datos").innerHTML = "No has introducido tu nombre y tu password";
+        document.getElementById("datos").style.borderBottom = "3px solid white";
     }
 }
